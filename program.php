cae593ab4c6dd74ff77091e17e378b9bbaf247a8
Abdul-Fuseni <abdul.fuseini@ashesi.edu.gh>
2020-12-10 17:20:22 +0000

    new file

--- program.php
+++ program.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>3 0
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Office of Diversity & International Programs</title>
-    <link rel="stylesheet" type="text/css" href="css/staff.css">
+    <link rel="stylesheet" type="text/css" href="css/program.css">
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -28,7 +28,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="index.html">Home</a>
                     </li>
-                    <li class="nav-item onsite">
+                    <li class="nav-item">
                         <a class="nav-link" href="staff.html">Staff</a>
                     </li>
                     <li class="nav-item">
@@ -37,7 +37,7 @@
                     <li class="nav-item">
                         <a class="nav-link" href="partner.php" >Partnerships</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item onsite">
                         <a class="nav-link" href="program.php">Programs</a>
                     </li>
                 </ul>
@@ -47,39 +47,60 @@
     </header>
     <section class="section-tutors js--section-features" id="section">
         <div class="container">
-            <h2 style="margin-top: 30px;"><center>A Pan African Campus</center></h2>
-            <p class="long-text">The Office of Diversity and International Programs (ODIP) coordinates and promotes all external collaborations and partnerships of the University as well as driving initiatives and programs aimed at advancing cross-cultural learning, awareness and acceptance of diversity and inclusion as a core tenant of the University.</p>
+            <h2 style="margin-top: 30px;"><center>Study Abroad & Partners</center></h2>
+            <p class="long-text">All Ashesi students who wish to enroll in an International Exchange Program and transfer academic credit back to Ashesi MUST complete a Study Abroad Application (click here to access the form) and submit it to the Office of Diversity and International Programs by the program deadline at odip@ashesi.edu.gh.  Students should apply as early as possible, or they risk not obtaining a visa in time. </p>
             <hr>
-            <ul class="list-unstyled">
-                <li class="media">
-                    <img src="img/mili.JPG" class="mr-3" alt="..." class="img">
-                    <div class="media-body">
-                      <h5 class="mt-0 mb-1">Dr. Milicent Adjei</h5>
-                      Director, Diversity & International Programs.
-                      <div>Radichel Hall 210</div>
-                    </div>
-                </li>
-                <hr>
-                <li class="media my-4">
-                    <img src="img/rose.PNG" class="mr-3" alt="...">
-                    <div class="media-body">
-                      <h5 class="mt-0 mb-1">Rosemary Kotei Buckman</h5>
-                      Assistant Director, International Programs
-                      <div>Radichel Hall 207</div>
-                    </div>
-                </li>
-                <hr>
-                <li class="media">
-                    <img src="img/vanessa.PNG" class="mr-3" alt="...">
-                    <div class="media-body">
-                      <h5 class="mt-0 mb-1">Vanessa Amoako</h5>
-                      Divertion & Inclusion Coordinator
-                      <div>Radichel Hall 207</div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+        </div>        
     </section>
+    
+    
+    <?php 
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $database = "AAF18942022";
+        $conn = mysqli_connect($servername, $username, $password, $database);
+        if (!$conn) {
+            die('Connection failed: ' . mysqli_connect_error());
+        }
+
+        $query = "SELECT * FROM Partnership";
+        echo "<b> <center>Study Abroad Partners</center> </b> <br> <br>";
+
+        echo '<table border="0" cellspacing="2" cellpadding="2"> 
+            <tr> 
+                <td> <font face="Arial"></font> </td> 
+                <td> <font face="Arial">Partners</font> </td> 
+                <td> <font face="Arial">Program</font> </td> 
+                <td> <font face="Arial"></font> </td>
+            </tr>';
+
+        if ($result = mysqli_query($conn, $query)) {
+
+            while ($row = $result->fetch_assoc()) {
+                $field1name = $row["partnership_id"];
+                $field2name = $row["partnership_name"];
+                $field3name = $row["program"];
+               
+
+                echo '<tr> 
+                        <td>'.$field1name.'</td> 
+                        <td>'.$field2name.'</td> 
+                        <td>'.$field3name.'</td>
+                      </tr>';
+        
+            }
+
+        /*freeresultset*/
+        $result->free();
+        }
+     ?>
+
+
+    <!-- <?php
+        include "register.php";                 
+    ?> -->
+
     <footer>
         <div class="footer-whole">
             <div class="container">
@@ -111,4 +132,3 @@
 
 </body>
 </html>
-
